0b0a71c3821fd801d899623eb4b4d69e5891bbdc
Jaret Houser <housjm02@students.ipfw.edu>
2016-11-28 18:36:46 -0500

    added error message for price input

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 /**
  * Created by jhous on 10/31/2016.
  */
-function DisplayHotel(){
+const max_hotel_price = 800;
+const max_flight_price= 1500;
 
+
+
+
+function CheckPrices(){
+    var maxPriceHotel = document.getElementById("maxPriceHotel").value;
+    if (IsValid(maxPriceHotel)){
+        maxPriceHotel = parseFloat(maxPriceHotel);
+        if(maxPriceHotel>= max_hotel_price){
+            SetError(maxPriceHotel, "Hotel price must be less than " +max_hotel_price);
+            return;
+        }
+    }else{
+        SetError(maxPriceHotel, "Input is not valid");
+        return;
+    }
+    var maxPriceFlight = document.getElementById("maxPriceFlight").value;
+    if (IsValid(maxPriceFlight)){
+        maxPriceFlight = parseFloat(maxPriceFlight);
+        if(maxPriceFlight>= max_flight_price){
+            SetError(maxPriceFlight, "Flight price must be less than " +max_flight_price);
+            return;
+        }
+    }else{
+        SetError(maxPriceFlight, "Input is not valid");
+        return;
+    }
+}
+function SetError(element, message){
+    document.getElementById("divOutput").innerHTML='*' + message;
+}
+function IsValid(value){
+    var valid = true;
+    if (isNaN(value)|| value ==""){
+        valid = false;
+    }
+    return valid;
 }

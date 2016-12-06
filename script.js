45a34a28ef9346ab25a7c8c827f4b88a5551ed9d
Jaret Houser <housjm02@students.ipfw.edu>
2016-12-06 18:48:31 -0500

    added storage for login and other input data

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
  */
 const max_hotel_price = 800;
 const max_flight_price= 1500;
-var maxPriceHotel = document.getElementById("maxPriceHotel").value;
+
 
 
 
 function CheckPrices(){
-
+    var maxPriceHotel = document.getElementById("maxPriceHotel").value;
     if (IsValid(maxPriceHotel)){
         maxPriceHotel = parseFloat(maxPriceHotel);
         if(maxPriceHotel>= max_hotel_price){
@@ -45,7 +45,7 @@ function $(x){
     return document.getElementById(x);
 }
 function DisplayHotel(){
-    if (document.getElementById("hongKong").checked){
+    if (localStorage.getItem("hotelPrice")<300&&(localStorage.getItem("selectedDest").value='hongKong')){
         document.getElementById('hotel2').style.display='none';
         document.getElementById('hotel3').style.display='none';
         document.getElementById("hotel4").style.display='none';
@@ -53,4 +53,40 @@ function DisplayHotel(){
         document.getElementById("hotel6").style.display='none';
         document.getElementById("hotel7").style.display='none';
     }
+}
+function StoreName(){
+    var userName=$("username").value;
+    localStorage.setItem('userName', userName);
+    var password=$("password").value;
+    localStorage.setItem('password', password);
+}
+function StoreSelections(){
+    var destination=document.getElementsByName('destination');
+    for (var i = 0; i < destination.length; i++) {
+        if (destination[i].checked==true) {
+            selectedDest=destination[i].value;
+            localStorage.setItem('selectedDest', selectedDest);
+        }}
+    var hotelPrice=$("maxPriceHotel").value;
+    localStorage.setItem('hotelPrice', hotelPrice);
+
+        var flightPrice=$("maxPriceFlight").value;
+        localStorage.setItem('flightPrice', flightPrice);
+}
+function DisplayName(){
+if(localStorage.getItem('userName')){
+    $("usersName").innerHTML="Hello  "+localStorage.getItem('userName');
+}
+}
+function CheckName(){
+    var storedName = localStorage.getItem('userName');
+    var storedPass = localStorage.getItem('password');
+    var userName = $('name');
+    var userPass = $('pass');
+
+    if(userName.value == storedName && userPass.value == storedPass) {
+    alert('You are logged in.');
+}else {
+    alert('Please try logging in again.');
+}
 }
